af9b09eb208fdcd585f5a8bb83625ab394eb1b58
shekolla <shekollasaikiran@gmail.com>
2020-02-27 23:27:20 +0530

    [_2ndLargest] Code for second largest in arr

---
+++ second_largest_number_in_array.py
@@ -0,0 +1,29 @@
+'''
+    Second Largest Number In Array
+'''
+
+from python_2_3_compatibilty import (input, map, print_function)
+
+def secondLargest(arr):
+    _1stHigh = _2ndHigh = float('-inf') # Assign lowest value possible
+
+    for each in arr:
+        if each > _1stHigh:
+            _2ndHigh = _1stHigh
+            _1stHigh = each
+        elif each > secondLargest:
+            _2ndHigh = each
+    
+    return _2ndHigh
+
+if __name__ == "__main__":
+    arr = list(map(lambda x: int(x), input().split()))
+    print secondLargest(arr)
+
+
+'''
+    input:
+        12 56 78 98 17 108
+    output:
+        98
+'''
